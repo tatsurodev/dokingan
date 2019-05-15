0586d58ab008faa60136da4259d9a8d355b7c6a4
Tatsuro <contacts@tatsuro.dev>
2019-05-16 08:07:42 +0900

    sexesテーブル作成
    性別マスタとusersテーブルとのリレーション定義

---
+++ database/migrations/2019_05_15_091947_create_sexes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSexesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sexes', function (Blueprint $table) {
+            $table->unsignedInteger('id')->primary();
+            $table->string('name', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sexes');
+    }
+}

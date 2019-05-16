6e3810b7d2b0a4452f73c08c2fcd435ad7b4bef1
Tatsuro <contacts@tatsuro.dev>
2019-05-16 12:12:16 +0900

    Userファクトリー作成

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,11 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\User;
+
+// 画像処理に使用
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -18,7 +23,18 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        // 10文字以内に丸める
+        'username' => substr($faker->username, 0, 10),
+        // sexesテーブルからラムダム
+        'sex_id' => \DB::table('sexes')->pluck('id')->random(),
+        // 一般ユーザーのロール
+        'role_id' => \DB::table('roles')->where('role_name', 'user')->first()->id,
+        'age' => rand(0, 100),
+        // $faker->imageでtmpファイルに保存後、Storage::putFileでstorage/app/public以下のフォルダに保存
+        'image' => Storage::putFile('users/profiles', new File($faker->image('/tmp', rand(200, 300), rand(200, 300), 'people'))),
+        'comment' => $faker->realText(rand(10, 255)),
+        'sph' => $faker->randomFloat(null, -15, 15),
+        'pd' => $faker->numberBetween(40, 100),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
